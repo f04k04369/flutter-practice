82c2839d7a4a016fdbc54d68febdf0b76ea9f629
f04k04369 <curb369@gmail.com>
2025-05-13 01:05:38 +0900

    ListViewの練習

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -46,37 +46,14 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: [
-                SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: ColoredBox(
-                    color: Colors.blue,
-                    )
-                ),
-                SizedBox(
-                  width: 200,
-                  height: 330,
-                  child: ColoredBox(
-                    color: Colors.red,
-                    )
-                ),
-              ],
+      body: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Item $index'),
+                );
+              },
             ),
-            const Text('You have pushed the button this many times:'),
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
