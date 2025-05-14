56acfcebdf947e72ec36a02ba63d5feaa4522d42
f04k04369 <curb369@gmail.com>
2025-05-14 17:15:19 +0900

    API通信の練習

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -55,55 +56,94 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _counter2 = 1;
+  String _text = '';
+  String text = '';
 
-  List<String> favorite = ["ガンダム","ドム","ザク","ジオング","EZ-8"];
+  List<String> favorite = ["ガンダム", "ドム", "ザク", "ジオング", "EZ-8"];
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+  void _twice() {
+    setState(() {
+      _counter2 = _counter2 * 2;
+    });
+  }
+
+  Future<void> getRepos() async {
+    // APIでgetするので非同期処理
+    final url = Uri.https('api.github.com', 'users/f04k04369/repos');
+    final response = await http.get(url);
+    print(response.body);
+    setState(() {
+      text = response.body;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('足し算'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const Text('2倍'),
+            Text(
+              '$_counter2',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  _text = value;
+                });
+              },
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  text = _text;
+                });
+              },
+               icon: Icon(Icons.arrow_downward)
+            ),
+            Text(text),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _twice,
+            tooltip: 'twice',
+            child: const Icon(Icons.heart_broken),
+          ),
+          FloatingActionButton(
+            onPressed: getRepos,
+            tooltip: 'api',
+            child: const Icon(Icons.heart_broken),
+          ),
+        ],
+      ),
     );
   }
 }
